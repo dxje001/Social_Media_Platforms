774c13055b1503eb3c6d8b7cb6490734370c3070
Dušan Stefanoivć <stefanovic.dusan001@outlook.com>
2024-12-08 19:05:48 +0100

    Namesten mail
    Namesten mail

--- backend/app/app.py
+++ backend/app/app.py
@@ -26,7 +26,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL '] = False
     app.config['MAIL_USERNAME'] = 'sreckomarinkovic000@gmail.com'  # Postavite vaš email
-    app.config['MAIL_PASSWORD'] = 'ovojesifra123'  # Postavite vašu lozinku
+    app.config['MAIL_PASSWORD'] = 'rssw cbpe uten qjgh'  # Postavite vašu lozinku
     app.config['MAIL_DEFAULT_SENDER'] = 'sreckomarinkovic000@gmail.com'
 
     db.init_app(app)
